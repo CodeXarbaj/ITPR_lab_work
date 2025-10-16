4da79b53d1945429c4faab37ce5e796442820c1e
Arbaj <tumharaemail@example.com>
2025-10-16 16:34:54 +0530

    Updated Java programs

--- AdditionExample.java
+++ AdditionExample.java
@@ -1,23 +1,23 @@
 /*-- Program to find out Addition of two numbers when first number is less than second number --*/
 
 import java.util.Scanner;
-class AdditionExample {
+class AdditionExample {    			//class of addition example
 
-	public static void main(String[] args) 
+	public static void main(String[] args) 		//main function
 	{
-		int num1,num2;
+		int num1,num2;   //Intialization of variable
 		//Creating object of scanner class
-		Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);  // Create sc for taking input
 		//Input of Numbers
 		System.out.println("Enter any two number : ");
-		num1 = sc.nextInt();
-		num2 = sc.nextInt();
+		num1 = sc.nextInt(); // Taking input
+		num2 = sc.nextInt(); // Taking input
 		System.out.println("-----------------------------------------------");
 		//Displaying number to the user
 		System.out.println("First No. : "+num1);
 		System.out.println("Second No. : "+num2);
 		/*---- finding Addition if num1 is less than num2 ----*/
-		if(num1 < num2)
+		if(num1 < num2)  //Checking condition if number 1 is less than number 2
 		{
 			System.out.println("Addition : "+(num1+num2));
 		}
